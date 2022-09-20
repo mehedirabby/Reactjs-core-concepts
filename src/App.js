f7b005e1ea9191c4fad2b3e0e2738460486790de
unknown <mehedi15-2995@diu.edu.bd>
2022-09-20 10:08:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers =[{name:'Dr Mahfuz',job:'singer'},{name:'Eva Rahman',job:'singer'},{name:'agun',job:'gan gay'},{name:'arnob',job:'pade'}]
+const singerStyle ={
+  color: 'purple',
+  backgroundColor:'darkBlue'
+
+}
+
 function App() {
+  const nayoks=['rubel','bappa da','kubel','sakib','joshim','salman sah','riaz','rajjak']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok=><li>Name:{nayok}</li>)
+      }
+      {
+        singers.map(singer=><Person name={singer.name}></Person> )
+      }
+      {/* {
+        nayoks.map(nayok=><Person name={nayok}></Person>)
+      } */}
+
+      {/* <Person name={nayoks[0]} nayika="samanta"></Person>
+      <Person name={nayoks[1]}  nayika="none"></Person>
+      <Person name={nayoks[2]} nayika="kapila"></Person> */}
+      
+      <h5>New Component yea</h5>
+      <img src="" alt="" />
+      <p className=''>Look I can do react with my own</p>
+      <Friend movie="singham" phone="01747777972"></Friend>
+      <Friend phone="01935215443"></Friend>
+      
+
+      
     </div>
   );
+}
+
+function Person(props){
+  
+  return (
+    <div className="person">
+    <h3>{props.name}</h3>
+    
+  </div>
+  )
+}
+function Friend (props){
+  console.log(props)
+  return(
+    <div className='container'>
+      <h3>Name:Ajay devgun</h3>
+      <p>Phone:{props.phone}</p>
+      <p>Movie:{props.movie}</p>
+    </div>
+  )
 }
 
 export default App;
